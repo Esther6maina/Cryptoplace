4d9baf021be72b8911418e37fb9e2925f9ca17b3
Esther Maina <estanyambura99@gmail.com>
2024-08-25 17:30:59 +0300

    initial commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,9 +9,16 @@ const Home = () => {
         <p>Welcome to the world's largest Cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
         <form>
           <input type="text" placeholder='Search crypto..'/>
-          <button type="submit">Search</button>
+          <button type="submit"> Search</button>
         </form>
       </div>
+      <div className="crtpto-table"></div>
+      <div className="table-layout"></div>
+      <p>#</p>
+      <p>Coins</p>
+      <p>Price</p>
+      <p>24H Change</p>
+      <p>Market Cap</p>
       
     </div>
   )
