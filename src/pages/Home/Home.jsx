f526697092499d5bea08be73c652657d2e374c90
Esther Maina <estanyambura99@gmail.com>
2024-09-10 19:44:48 +0300

    coingecko

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import './Home.css'
+import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div className="home">
       <div className="hero">
-        <h1>Largest<br/>Crypto Marketplace</h1>
-        <p>Welcome to the world's largest Cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
+        <h1>Largest<br />Crypto Marketplace</h1>
+        <p>
+          Welcome to the world's largest Cryptocurrency marketplace. Sign up to explore more about cryptos.
+        </p>
         <form>
-          <input type="text" placeholder='Search crypto..'/>
-          <button type="submit"> Search</button>
+          <input type="text" placeholder="Search crypto..." />
+          <button type="submit">Search</button>
         </form>
       </div>
-      <div className="crtpto-table"></div>
-      <div className="table-layout"></div>
-      <p>#</p>
-      <p>Coins</p>
-      <p>Price</p>
-      <p>24H Change</p>
-      <p>Market Cap</p>
-      
-    </div>
-  )
-}
 
-export default Home
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{ textAlign: 'center' }}>24H Change</p>
+          <p className="market-cap">Market Cap</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
